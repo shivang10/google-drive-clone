242e8e801c79b3574a784055ba8f41fb53dea225
Shivang Garg <shivang10garg@gmail.com>
2021-03-07 20:18:47 +0530

    user auth added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/header/index";
 import Sidebar from "./components/sideBar/index";
 import FilesView from "./components/filesView/filesView";
 import SideBarIcons from "./components/sideIcons";
+import GDriveLogo from "./media/google-drive-logo.png"
+import {auth, provider} from "./firebase";
 
 function App() {
 
@@ -13,15 +15,41 @@ function App() {
         emailVerified: true,
         phoneNumber: null,
         photoURL: "https://lh6.googleusercontent.com/-KyLTWqvDIHQ/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclcWGWqkt6YUAan32YO4CSR07Y2jw/s96-c/photo.jpg"
-    })
+    });
+
+    const handleLogin = () => {
+        if (!user) {
+            auth.signInWithPopup(provider).then((result) => {
+                setUser(result.user)
+                console.log(result.user)
+            }).catch((error) => {
+                alert(error.message);
+            });
+        } else if (user) {
+            auth.signOut().then(() => {
+                setUser(null)
+            }).catch((err) => alert(err.message))
+        }
+    }
     return (
-        <div className="App">
-            <Header userPhoto={user.photoURL}/>
-            <div className="app__main">
-                <Sidebar/>
-                <FilesView/>
-                <SideBarIcons />
-            </div>
+        <div className="app">
+            {
+                user ? (
+                    <>
+                        <Header userPhoto={user.photoURL}/>
+                        <div className="app__main">
+                            <Sidebar/>
+                            <FilesView/>
+                            <SideBarIcons/>
+                        </div>
+                    </>
+                ) : (
+                    <div className='app__login'>
+                        <img src={GDriveLogo} alt="Google Drive"/>
+                        <button onClick={handleLogin}>Log in to Google Drive</button>
+                    </div>
+                )
+            }
         </div>
     );
 }
