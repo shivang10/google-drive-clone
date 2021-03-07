4835b47175a5df84fa2b87f87dae082e3847a827
Shivang Garg <shivang10garg@gmail.com>
2021-03-07 20:06:53 +0530

    file view styles updated, file card added

--- src/components/filesView/filesView.js
+++ src/components/filesView/filesView.js
@@ -1,7 +1,9 @@
 import React,{useState, useEffect} from "react";
 import {db} from "../../firebase";
+import "../../styles/filesView.css";
 
 import FileItem from "./FileItem";
+import FileCard from "./fileCard";
 
 const FilesView = () => {
 
@@ -19,13 +21,17 @@ const FilesView = () => {
     return (
         <div className="filesView">
             <div className="filesView__row">
-
+                {
+                    files.slice(0, 5).map(({id, item}) => (
+                        <FileCard name={item.caption} />
+                    ))
+                }
             </div>
             <div className="filesView__titles">
-                <div className="fileView__titles--left">
+                <div className="filesView__titles--left">
                     <p>Name</p>
                 </div>
-                <div className="fileView__tiles--right">
+                <div className="filesView__titles--right">
                     <p>Last Modified</p>
                     <p>Files size</p>
                 </div>
